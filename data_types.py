f22dd5b1d0c1322f7671fc4c7cc5c071562ae1de
thansiha <thansiha1223@gmail.com>
2026-09-03 10:16:22 +0530

    first commit

---
+++ data_types.py
@@ -0,0 +1,14 @@
+name ="Thansiha"
+age = 18
+height = 5.6
+is_student = True
+
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Is_student):", is_student)
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(height))
