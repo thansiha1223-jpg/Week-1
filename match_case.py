6a91508e211967527ea621c4b5681746dbe2ea92
thansiha <thansiha1223@gmail.com>
2026-09-04 08:38:39 +0530

    updated match_case

---
+++ match_case.py
@@ -0,0 +1,11 @@
+day = 2
+
+match day:
+    case 1:
+        print("Monday")
+    case 2:
+        print("Tuesday")
+    case 3:
+        print("Wednesday")
+    case _:
+        print("Invalid day")
