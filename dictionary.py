68272adee5d744a7b42f018135b11947121c4c2f
thansiha <thansiha1223@gmail.com>
2026-09-05 13:59:31 +0530

    updated dictionary

---
+++ dictionary.py
@@ -0,0 +1,12 @@
+student = {
+    "name": "Thansiha",
+    "age": 18,
+    "course": "Python"
+}
+
+print("Student:", student)
+print("Name:", student["name"])
+
+student["city"] = "Kerala"
+
+print("Updated student:", student)
